2bc5a8864c3019f9c2a0bef2e9d9e9500277b2ac
Juan Rodriguez <emirodriguez_025@hotmail.com>
2022-05-23 14:58:01 -0300

    terminamos navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import logo from '../assests/logo.jpg'
+import { ShoppingCart } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: "7rem",
+  },
+  appBar: {
+    backgroundColor: "whitesmoke",
+    boxShadow: "none",
+  },
+  grow: {
+     flexGrow: 1,
+  },
+  button: {
+    marginLeft: theme.spacing(2),
+  },
+  image: {
+    marginRight: "10px",
+    height: "3rem",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar} >
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <img src={logo} className={classes.image} />
+          </IconButton>
+          <div className={classes.grow} />
+          <Typography variant="h6" color="textPrimary" component="p">
+            Hello Guest
+          </Typography>
+          <div className={classes.button} >       
+            <Button variant="outlined">
+              <strong>Sign in</strong>
+            </Button>
+            <IconButton aria-label='show cart items' color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCart fontSize="large" color="primary" />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}

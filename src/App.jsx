0f15c011c52c092dcf1300f060c4b06e8a83d73e
Juan Rodriguez <emirodriguez_025@hotmail.com>
2022-05-23 17:05:27 -0300

    agregué archivos CheckoutPage y CheckoutCard falta funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Product from './components/Product';
 import Products from './components/Products';
+import CheckoutPage from './components/CheckoutPage'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      {/* <CheckoutPage/> */}
       <Products/>
       {/* <Product/> */}
     </div>
---
+++ src/components/CheckoutPage.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import products from '../product-data';
+import CheckoutCard from "./CheckoutCard";
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: "2rem",
+  }
+}));
+
+const CheckoutPage = () => {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+          {products.map(product => (
+              <Grid item xs={12} sm={8} md={6} lg={4}>
+                <CheckoutCard key={product.id} product={product} />
+              </Grid>
+              ))}
+      </React.Fragment>
+    );    
+  }
+
+return (
+  <div className={classes.root}>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography align="center" gutterBottom variant="h4">
+          Shopping Cart
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={8} md={9} container spacing={2}>
+        <FormRow/>
+      </Grid>  
+
+      <Grid item xs={12}>
+        <Typography align="center" gutterBottom variant="h4">
+          Total
+        </Typography>
+      </Grid>
+    </Grid>
+  </div>
+);
+};
